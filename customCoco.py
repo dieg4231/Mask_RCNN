06babd76f196b29c40a937f9be6bdd7adcf2b4ad
dieg4231 <dieg4231@gmail.com>
2019-12-10 12:19:09 -0600

    mod

---
+++ customCoco.py
@@ -0,0 +1,289 @@
+"""
+Mask R-CNN
+Train on the toy Balloon dataset and implement color splash effect.
+
+Copyright (c) 2018 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+
+------------------------------------------------------------
+
+Usage: import the module (see Jupyter notebooks for examples), or run from
+       the command line as such:
+
+    # Train a new model starting from pre-trained COCO weights
+    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=coco
+
+    # Resume training a model that you had trained earlier
+    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=last
+
+    # Train a new model starting from ImageNet weights
+    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=imagenet
+
+    # Apply color splash to an image
+    python3 balloon.py splash --weights=/path/to/weights/file.h5 --image=<URL or path to file>
+
+    # Apply color splash to video using the last weights you trained
+    python3 balloon.py splash --weights=last --video=<URL or path to file>
+"""
+# py2/3 compability
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import skimage.draw
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath(".")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mmrcnn.config import Config
+from mmrcnn import model as modellib, utils
+
+# Path to trained weights file
+COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mobile_mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+############################################################
+#  Configurations
+############################################################
+
+
+class CustomCocoConfig(Config):
+    """Configuration for training on the toy  dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "tableware"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 36  # Background + balloon
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 100
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.1
+
+    BACKBONE = "resnet50"
+
+    # All of our training images are 512x512
+    #IMAGE_MIN_DIM = 512
+    #IMAGE_MAX_DIM = 512
+
+    # Matterport originally used resnet101, but I downsized to fit it on my graphics card
+    #BACKBONE = 'mobilenetv1'
+    BACKBONE = 'resnet50'
+
+    # To be honest, I haven't taken the time to figure out what these do
+    #RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+    #TRAIN_ROIS_PER_IMAGE = 32
+    #MAX_GT_INSTANCES = 50 
+    #POST_NMS_ROIS_INFERENCE = 500 
+    #POST_NMS_ROIS_TRAINING = 1000
+    USE_MULTIPROCESSING = True
+
+
+############################################################
+#  Dataset
+############################################################
+class CocoLikeDataset(utils.Dataset):
+    """ Generates a COCO-like dataset, i.e. an image dataset annotated in the style of the COCO dataset.
+        See http://cocodataset.org/#home for more information.
+    """
+    def load_data(self, annotation_json, images_dir):
+        """ Load the coco-like dataset from json
+        Args:
+            annotation_json: The path to the coco annotations json file
+            images_dir: The directory holding the images referred to by the json file
+        """
+        # Load json from file
+        json_file = open(annotation_json)
+        coco_json = json.load(json_file)
+        json_file.close()
+        
+        # Add the class names using the base method from utils.Dataset
+        source_name = "coco_like"
+        for category in coco_json['categories']:
+            class_id = category['id']
+            class_name = category['name']
+            if class_id < 1:
+                print('Error: Class id for "{}" cannot be less than one. (0 is reserved for the background)'.format(class_name))
+                return
+            
+            self.add_class(source_name, class_id, class_name)
+        
+        # Get all annotations
+        annotations = {}
+        for annotation in coco_json['annotations']:
+            image_id = annotation['image_id']
+            if image_id not in annotations:
+                annotations[image_id] = []
+            annotations[image_id].append(annotation)
+        
+        # Get all images and add them to the dataset
+        seen_images = {}
+        for image in coco_json['images']:
+            image_id = image['id']
+            if image_id in seen_images:
+                print("Warning: Skipping duplicate image id: {}".format(image))
+            else:
+                seen_images[image_id] = image
+                try:
+                    image_file_name = image['file_name']
+                    image_width = image['width']
+                    image_height = image['height']
+                except KeyError as key:
+                    print("Warning: Skipping image (id: {}) with missing key: {}".format(image_id, key))
+                
+                image_path = os.path.abspath(os.path.join(images_dir, image_file_name))
+                image_annotations = annotations[image_id]
+                
+                # Add the image using the base method from utils.Dataset
+                self.add_image(
+                    source=source_name,
+                    image_id=image_id,
+                    path=image_path,
+                    width=image_width,
+                    height=image_height,
+                    annotations=image_annotations
+                )
+                
+    def load_mask(self, image_id):
+        """ Load instance masks for the given image.
+        MaskRCNN expects masks in the form of a bitmap [height, width, instances].
+        Args:
+            image_id: The id of the image to load masks for
+        Returns:
+            masks: A bool array of shape [height, width, instance count] with
+                one mask per instance.
+            class_ids: a 1D array of class IDs of the instance masks.
+        """
+        image_info = self.image_info[image_id]
+        annotations = image_info['annotations']
+        instance_masks = []
+        class_ids = []
+        
+        for annotation in annotations:
+            class_id = annotation['category_id']
+            mask = Image.new('1', (image_info['width'], image_info['height']))
+            mask_draw = ImageDraw.ImageDraw(mask, '1')
+            for segmentation in annotation['segmentation']:
+                mask_draw.polygon(segmentation, fill=1)
+                bool_array = np.array(mask) > 0
+                instance_masks.append(bool_array)
+                class_ids.append(class_id)
+
+        mask = np.dstack(instance_masks)
+        class_ids = np.array(class_ids, dtype=np.int32)
+        
+        return mask, class_ids
+
+
+def color_splash(image, mask):
+    """Apply color splash effect.
+    image: RGB image [height, width, 3]
+    mask: instance segmentation mask [height, width, instance count]
+
+    Returns result image.
+    """
+    # Make a grayscale copy of the image. The grayscale copy still
+    # has 3 RGB channels, though.
+    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
+    # Copy color pixels from the original color image where mask is set
+    if mask.shape[-1] > 0:
+        # We're treating all instances as one, so collapse the mask into one layer
+        mask = (np.sum(mask, -1, keepdims=True) >= 1)
+        splash = np.where(mask, image, gray).astype(np.uint8)
+    else:
+        splash = gray.astype(np.uint8)
+    return splash
+
+
+def detect_and_color_splash(model, video_path=None):
+    assert video_path
+
+    import cv2
+    # Video capture
+    vcapture = cv2.VideoCapture(video_path)
+    width = int(vcapture.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = vcapture.get(cv2.CAP_PROP_FPS)
+
+    # Define codec and create video writer
+    file_name = "splash_{:%Y%m%dT%H%M%S}.avi".format(datetime.datetime.now())
+    #vwriter = cv2.VideoWriter(file_name,
+    #                              cv2.VideoWriter_fourcc(*'MJPG'),
+    #                              fps, (width, height))
+
+    count = 0
+    success = True
+    while success:
+    	print("frame: ", count)
+        # Read next image
+        success, image = vcapture.read()
+        if success:
+        	# OpenCV returns images as BGR, convert to RGB
+                image = image[..., ::-1]
+                # Detect objects
+                r = model.detect([image], verbose=0)[0]
+                # Color splash
+                splash = color_splash(image, r['masks'])
+                # RGB -> BGR to save image to video
+                splash = splash[..., ::-1]
+                # Add image to video writer
+                cv2.imshow('Mask RCNN', splash)
+                cv2.waitKey(1)
+                count += 1
+    #vwriter.release()
+    print("Saved to ", file_name)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Mask R-CNN to detect balloons.')
+    parser.add_argument('--weights', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    parser.add_argument('--video', required=False,
+                        metavar="path or URL to video",
+                        help='Video to apply the color splash effect on')
+    args = parser.parse_args()
+
+    print("Weights: ", args.weights)
+    print("Logs: ", args.logs)
+
+    class InferenceConfig(CustomCocoConfig):
+    	GPU_COUNT = 1
+    	IMAGES_PER_GPU = 1
+    	DETECTION_MIN_CONFIDENCE = 0.65
+    config = InferenceConfig()
+    config.display()
+
+    model = modellib.MaskRCNN(mode="inference", config=config, model_dir=args.logs)
+    weights_path = args.weights
+
+    # Load weights
+    print("Loading weights ", weights_path)
+    model.load_weights(weights_path, by_name=True)
+    detect_and_color_splash(model, video_path=args.video)
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 """
 The build/compilations setup
-
 >> pip install -r requirements.txt
 >> python setup.py install
 """
@@ -39,11 +38,11 @@ setup(
     author_email='waleed.abdulla@gmail.com',
     license='MIT',
     description='Mask R-CNN for object detection and instance segmentation',
-    packages=["mrcnn"],
+    packages=["mmrcnn"],
     install_requires=install_reqs,
     include_package_data=True,
-    python_requires='>=3.4',
-    long_description="""This is an implementation of Mask R-CNN on Python 3, Keras, and TensorFlow. 
+    python_requires='==2.7',
+    long_description="""This is an implementation of Mask R-CNN on Python 2 Jetson, Keras, and TensorFlow. 
 The model generates bounding boxes and segmentation masks for each instance of an object in the image. 
 It's based on Feature Pyramid Network (FPN) and a ResNet101 backbone.""",
     classifiers=[
@@ -60,9 +59,7 @@ It's based on Feature Pyramid Network (FPN) and a ResNet101 backbone.""",
         "Topic :: Scientific/Engineering :: Image Recognition",
         "Topic :: Scientific/Engineering :: Visualization",
         "Topic :: Scientific/Engineering :: Image Segmentation",
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 2.7',
     ],
     keywords="image instance segmentation object detection mask rcnn r-cnn tensorflow keras",
 )
